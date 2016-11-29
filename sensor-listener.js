43430bb23475a47882d92e00864e848b1d3cc1d9
cagdasyelen <cagdas@utexas.edu>
2016-11-28 18:06:39 -0600

    Sensor listener is changed

--- sensor-listener.js
+++ sensor-listener.js
@@ -1,19 +1,4 @@
-/*
-    SensorTag IR Temperature sensor example
-    This example uses Sandeep Mistry's sensortag library for node.js to
-    read data from a TI sensorTag.
-    The sensortag library functions are all asynchronous and there is a
-    sequence that must be followed to connect and enable sensors.
-      Step 1: Connect
-        1) discover the tag
-        2) connect to and set up the tag
-      Step 2: Activate sensors
-        3) turn on the sensor you want to use (in this case, IR temp)
-        4) turn on notifications for the sensor
-      Step 3: Register listeners
-        5) listen for changes from the sensortag
-      Step 4 (optional): Configure sensor update interval
-*/
+
 var SensorTag = require('sensortag');
 var mathjs = require('mathjs');
 
@@ -25,13 +10,9 @@ var log = function(text) {
 
 
 
-
-
 //==============================================================================
 // Step 1: Connect to sensortag device.
 //------------------------------------------------------------------------------
-// It's address is printed on the inside of the red sleeve
-// (replace the one below).
 var ADDRESS = "b0:b4:48:c9:09:85";
 var connected = new Promise((resolve, reject) => SensorTag.discoverByAddress(ADDRESS, (tag) => resolve(tag)))
   .then((tag) => new Promise((resolve, reject) => tag.connectAndSetup(() => resolve(tag))));
@@ -39,11 +20,6 @@ var connected = new Promise((resolve, reject) => SensorTag.discoverByAddress(ADD
 //==============================================================================
 // Step 2: Enable the sensors you need.
 //------------------------------------------------------------------------------
-// For a list of available sensors, and other functions,
-// see https://github.com/sandeepmistry/node-sensortag.
-// For each sensor enable it and activate notifications.
-// Remember that the tag object must be returned to be able to call then on the
-// sensor and register listeners.
 var sensor = connected.then(function(tag) {
   log("connected");
 
@@ -59,43 +35,20 @@ var sensor = connected.then(function(tag) {
 //==============================================================================
 // Step 3: Register listeners on the sensor.
 //------------------------------------------------------------------------------
-// You can register multiple listeners per sensor.
-//
-
-
-var rawArr = []
-var temp = []
-
 
 sensor.then(function(tag) {
   tag.on("accelerometerChange", function(x, y,z) {
-  	//console.log("Acc data: (x, y, z) = (" + x + ", " + y + ", " + z + " )"  + "Time: "+ Date.now());
-    //writeToFile(x + ","  + y + "," + z);
-    console.log(rawArr.length + "\n"); 
-    if(rawArr.length > 29){
-    	rawArr.shift();
-    }
-    temp.push(x);
-    temp.push(y);
-    temp.push(z);
+  	console.log("Acc data: (x, y, z) = (" + x + ", " + y + ", " + z + " )"  + "Time: "+ Date.now());
+    writeToFile(x + ","  + y + "," + z);
+
   })
 });
 
 
 sensor.then(function(tag) {
   tag.on("gyroscopeChange", function(x, y,z) {
-  	//console.log("Gyro  data: (x, y, z) = (" + x + ", " + y + ", " + z + " )" );
-    //writeToFile("," + x + "," + y + "," + z + "\n");
-    temp.push(x);
-    temp.push(y);
-    temp.push(z);
-    
-    temp = [];
-    rawArr.push(temp);
-
-    features = getStatistics(rawArr);
-    console.log("extracted features:");
-    console.log(features);
+  	console.log("Gyro  data: (x, y, z) = (" + x + ", " + y + ", " + z + " )" );
+    writeToFile("," + x + "," + y + "," + z + "\n");
 
   })
 });
@@ -103,133 +56,10 @@ sensor.then(function(tag) {
 //==============================================================================
 // Step 4 (optional): Configure periods for sensor reads.
 //------------------------------------------------------------------------------
-// The registered listeners will be invoked with the specified interval.
-//
-
 sensor.then(function(tag) {
 	  tag.setAccelerometerPeriod(100, log);
 	  tag.setGyroscopePeriod(100,log);
 });
-
-writeToFile = function(line){
-  var fs = require('fs');
-  fs.appendFile("/tmp/data.csv", line, function(err){
-    if(err){
-      return console.log(err);
-    }
-  });
-
-}
-/*
-classifier = function(data){
-  if(ed['aP2P'] <= 0.941){
-    return 0;
-  }
-  else{
-    if(ed['aAvg'] <= 1.2813){
-      if(ed['gAvg'] <=95.5936){
-        return 1;
-      }
-      else{
-        if(ed['aAvg'] <= 1.2155){
-          if(ed['aStd'] <= 0.6642){
-            if(ed['aAvg'] <= 0.7339){
-              return 2;
-            }
-            else{
-              if(ed['aMin'] <= 0.1213){
-                return 1;
-              }
-              else{
-                return 2;
-              }
-          }
-          else{
-            if(ed['gMax'] <= 367.7342){
-              return 1;
-            }
-            else{
-              return 2;
-            }
-          }
-        }
-        else{
-          return 1;
-        }
-      }
-    }
-    
-    else{
-      if(ed['aMin'] <= 0.0565){
-        if(ed['gAvg'] <= 257.9375){
-          if(ed['gMin'] <= 3.6016){
-            return 2;
-          }
-          else{
-            return 1;
-          }
-        }
-        else{
-          if(ed['gStd'] <= 133.4024){
-            return 2;
-          }
-          else{
-            return 3;
-          }
-        }
-      }
-      else{
-        if(ed['aMin'] <= 0.1215){
-          if(ed['gAvg'] <= 187.825){
-            return 2;
-          }
-          else{
-            return 3;
-          }
-        }
-        else{
-          return 3;
-        }
-      }
-    }
-
-  }
-}
-
-}
-*/
-
-var getStatistics = function(arr){
-  var len = arr.length;
-  var i=0;
-
-  var aMag = [];
-  var gMag = [];
-
-  for(i = 0 ; i < length; i++){
-    aMag.push(Math.sqrt(Math.pow(arr[i][0]) + Math.pow(arr[i][1]) + Math.pow(arr[i][2])));
-    gMag.push(Math.sqrt(Math.pow(arr[i][3]) + Math.pow(arr[i][4]) + Math.pow(arr[i][5])));
-  }
-
-
-  var aAvg =mathjs.mean(aMag);;
-  var aMax = mathjs.max(aMag);;
-  var aMin = mathjs.min(aMag);;
-  var aP2P = aMax - aMin;
-  var aStd = mathjs.std(aMag);
-  var gAvg = mathjs.mean(gMag);
-  var gMax =mathjs.max(gMag);
-  var gMin =mathjs.min(gMag);
-  var gP2P = gMax - gMin;
-  var gStd = mathjs.std(gMag);
-
-
-  return [aAvg, aMax, aMin, aP2P, aStd, gAvg, gMax, gMin, gP2P, gStd];
-
-
-}
-
-
 
 
 
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var server = http.createServer(function (request, response) {
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000,function(){
+
 	server.get
 	request({
     	url: 'http://ec2-54-212-222-6.us-west-2.compute.amazonaws.com:3000/fall', //URL to hit
@@ -21,8 +22,12 @@ server.listen(8000,function(){
 	        console.log(response.statusCode, body);
 	    }
 	});
+
 });
 
 
 //Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
+
+
+
